15c575c760779de47039e0910f10284056cb9b43
mgribb3n <magnus@gribbestad.no>
2016-09-07 20:30:02 +0200

    Lagt til klassen "Protocol". Dette er enum som gjør det mer oversiktlig når vi jobber med sending og mottak av data.

--- src/remotecontroller/UDPClient.java
+++ src/remotecontroller/UDPClient.java
@@ -5,10 +5,62 @@
  */
 package remotecontroller;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author mgrib
  */
 public class UDPClient {
+    private InetAddress IPAddress; // 158.38.199.18
+    private String ip;
+    private DatagramSocket clientSocket;
     
+    public UDPClient(String IpAddress){
+         this.ip = IpAddress;
+    }
+    
+    public void connect(){
+        try {
+            IPAddress = InetAddress.getByName(this.ip);
+            clientSocket = new DatagramSocket();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(UDPClient.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SocketException ex) {
+            Logger.getLogger(UDPClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    public void send(ArrayList<Boolean> data){
+        this.connect();
+        
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        ObjectOutputStream out;
+        try {
+            out = new ObjectOutputStream(buffer);
+            out.writeObject(data);
+            out.close();
+            buffer.close();
+            DatagramPacket packet = new DatagramPacket(buffer.toByteArray(), 
+                                        buffer.size(), 
+                                        IPAddress,
+                                        9876);
+            clientSocket.send(packet);
+            clientSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(UDPClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
