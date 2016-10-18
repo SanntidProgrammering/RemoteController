5434be41b7703b42b8df0585981756a510869ae1
mgribb3n <magnus@gribbestad.no>
2016-10-18 11:40:55 +0200

    Laget en fungerende versjon fra testerRemoteController prosjektet

--- src/remotecontroller/Main.java
+++ src/remotecontroller/Main.java
@@ -15,7 +15,7 @@ import org.opencv.core.Core;
  */
 public class Main {
 
-    static final String IPADDRESS = "192.168.0.100";//"158.38.199.15";  //"192.168.0.103"; //"158.38.199.107";//"10.16.5.58";//"10.16.5.119";//"158.38.199.107";//"192.168.0.10";
+    static final String IPADDRESS = "158.38.199.58";  // Jørg"10.16.5.58";
     static final int RECEIVEPORT = 5000;
     static final int SENDPORT = 9876;
     static SendEventState enumStateEvent = SendEventState.FALSE;
